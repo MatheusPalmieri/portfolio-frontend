0c7494760ecedb38e043c9d3fdbe3525056c32f3
Matheus Palmieri <matheuspalmieridev@gmail.com>
2024-02-18 19:10:45 -0300

    fix: id key

--- src/components/ProjectsCards/index.tsx
+++ src/components/ProjectsCards/index.tsx
@@ -14,7 +14,7 @@ export const ProjectsCards = ({ projects, loading }: ProjectsCardsProps) => {
     <Group spacing='md' align='start' style={{ justifyContent: 'center' }}>
       {loading
         ? [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((idx: number) => <ProjectCardLoader key={idx} />)
-        : projects.map((project: IProject) => <ProjectCard key={project._id} project={project} />)}
+        : projects.map((project: IProject) => <ProjectCard key={project.id} project={project} />)}
     </Group>
   );
 };
